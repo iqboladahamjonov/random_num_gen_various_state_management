d1b3fdbac94036b2e26d605c687afdf02f2164a6
Iqbol_Adahamjonov <iqbol.adahamjonov@gmail.com>
2021-09-15 22:08:56 +0500

    52 - Extracting-form-into-widget

--- lib/range_selector_page.dart
+++ lib/range_selector_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_foundations/range_selector_form.dart';
 
 class RangeSelectorPage extends StatefulWidget {
   const RangeSelectorPage({Key? key}) : super(key: key);
@@ -17,27 +18,10 @@ class _RangeSelectorPageState extends State<RangeSelectorPage> {
       appBar: AppBar(
         title: const Text('Select Range'),
       ),
-      body: Form(
-        key: formKey,
-        child: Container(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                RangeSelectorTextFormField(
-                  labelText: 'Minimum',
-                  intValueSetter: (value) => _min = value,
-                ),
-                const SizedBox(height: 12),
-                RangeSelectorTextFormField(
-                  labelText: 'Minimum',
-                  intValueSetter: (value) => _max = value,
-                ),
-              ],
-            ),
-          ),
-        ),
+      body: RangeSelectorForm(
+        formKey: formKey,
+        minValueSetter: (int value) => _min = value,
+        maxValueSetter: (int value) => _max = value,
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.arrow_forward),
@@ -48,37 +32,6 @@ class _RangeSelectorPageState extends State<RangeSelectorPage> {
           // Navigate the the generator page
         },
       ),
-    );
-  }
-}
-
-class RangeSelectorTextFormField extends StatelessWidget {
-  RangeSelectorTextFormField({
-    Key? key,
-    required this.labelText,
-    required this.intValueSetter,
-  }) : super(key: key);
-  final String labelText;
-  final void Function(int value) intValueSetter;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: labelText,
-      ),
-      keyboardType: const TextInputType.numberWithOptions(
-        signed: true,
-        decimal: false,
-      ),
-      validator: (value) {
-        if (value == null || int.tryParse(value) == null) {
-          return 'Must be an integer';
-        }
-        return null;
-      },
-      onSaved: (newValue) => intValueSetter(int.parse(newValue ?? '')),
     );
   }
 }
