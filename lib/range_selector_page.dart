01000b1bb14a5ec45498c86ff139393332b8cf52
Iqbol_Adahamjonov <iqbol.adahamjonov@gmail.com>
2021-09-16 08:38:52 +0500

    55 - Reimplementing with Flutter Hooks

--- lib/range_selector_page.dart
+++ lib/range_selector_page.dart
@@ -1,28 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_foundations/randomizer_page.dart';
 import 'package:flutter_foundations/range_selector_form.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class RangeSelectorPage extends StatefulWidget {
-  const RangeSelectorPage({Key? key}) : super(key: key);
-
-  @override
-  State<RangeSelectorPage> createState() => _RangeSelectorPageState();
-}
-
-class _RangeSelectorPageState extends State<RangeSelectorPage> {
+class RangeSelectorPage extends HookWidget {
   final formKey = GlobalKey<FormState>();
-  int _min = 0;
-  int _max = 0;
+
+  RangeSelectorPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    final min = useState<int>(0);
+    final max = useState<int>(0);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Select Range'),
       ),
       body: RangeSelectorForm(
         formKey: formKey,
-        minValueSetter: (int value) => _min = value,
-        maxValueSetter: (int value) => _max = value,
+        minValueSetter: (int value) => min.value = value, //it is setter, that is why min, max can be finals
+        maxValueSetter: (int value) => max.value = value,
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.arrow_forward),
@@ -31,8 +28,8 @@ class _RangeSelectorPageState extends State<RangeSelectorPage> {
             formKey.currentState?.save();
             Navigator.of(context).push(MaterialPageRoute(
                 builder: (context) => RandomizerPage(
-                      min: _min,
-                      max: _max,
+                      min: min.value,
+                      max: max.value,
                     )));
           }
         },
