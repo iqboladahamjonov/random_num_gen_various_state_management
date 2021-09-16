f3c869ec25f57512e306969999c83231e4e81c8f
Iqbol_Adahamjonov <iqbol.adahamjonov@gmail.com>
2021-09-16 10:02:41 +0500

    57 - Provider - Providing the ChangeNotifier around the app

--- lib/randomizer_page.dart
+++ lib/randomizer_page.dart
@@ -1,34 +1,40 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_foundations/randomizer_change_notifier.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/src/provider.dart';
 
-class RandomizerPage extends HookWidget {
-  final randomGenerator = Random();
-
-  final int min, max;
+class RandomizerPage extends StatelessWidget {
   RandomizerPage({
     Key? key,
-    required this.min,
-    required this.max,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final generatedNumber = useState<int?>(null);
+    // final generatedNumber = useState<int?>(null);
     return Scaffold(
       appBar: AppBar(
         title: Text('Randomizer'),
       ),
-      body: Center(
-        child: Text(
-          generatedNumber.value?.toString() ?? 'Generate a number', //used safe operator ?, coz number can be null still
-          style: TextStyle(fontSize: 42),
-        ),
-      ),
+      body: Center(child: Consumer<RandomizerChangeNotifier>(
+        builder: (context, notifier, child) {
+          return Text(
+            notifier.generatedNumber?.toString() ?? 'Generate a number',
+            style: TextStyle(fontSize: 42),
+          );
+        },
+      )
+          // child: Text(
+          //   // generatedNumber.value?.toString() ?? 'Generate a number', //used safe operator ?, coz number can be null still
+          //   context.read<RandomizerChangeNotifier>().generatedNumber?.toString() ?? 'Generate a number', //used safe operator ?, coz number can be null still
+          //   style: TextStyle(fontSize: 42),
+          // ),
+          ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
-          generatedNumber.value = min + randomGenerator.nextInt(max + 1 - min);
+          context.read<RandomizerChangeNotifier>().generateRandomNumber();
+          // generatedNumber.value = min + randomGenerator.nextInt(max + 1 - min);
           //   setState(() {
           //     _generatedNumber = widget.min + randomGenerator.nextInt(widget.max + 1 - widget.min);
           //   });
